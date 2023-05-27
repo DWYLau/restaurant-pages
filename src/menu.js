bbbd71aec74fe7ad1963e47bf0e7e6679a848793
Derek <dereklau15@gmail.com>
2023-05-27 20:55:26 +0900

    Add text overlay to images

--- src/menu.js
+++ src/menu.js
@@ -11,30 +11,59 @@ function createMenu() {
   const menu = document.createElement("div");
   menu.classList.add("menu-container");
 
-  const imageOne = document.createElement("img");
-  const imageTwo = document.createElement("img");
-  const imageThree = document.createElement("img");
-  const imageFour = document.createElement("img");
+  const div1 = document.createElement("div");
+  const div2 = document.createElement("div");
+  const div3 = document.createElement("div");
+  const div4 = document.createElement("div");
 
-  imageOne.classList.add("menu-image");
-  imageTwo.classList.add("menu-image");
-  imageThree.classList.add("menu-image");
-  imageFour.classList.add("menu-image");
+  const image1 = document.createElement("img");
+  const image2 = document.createElement("img");
+  const image3 = document.createElement("img");
+  const image4 = document.createElement("img");
 
-  imageOne.src = "./images/image1.jpg";
-  imageTwo.src = "./images/image2.jpeg";
-  imageThree.src = "./images/image3.jpg";
-  imageFour.src = "./images/image4.jpg";
+  const label1 = document.createElement("h2");
+  label1.innerHTML = "Sorrowful Rice";
+  label1.classList.add("label");
+
+  const label2 = document.createElement("h2");
+  label2.innerHTML = "Cart Noodle";
+  label2.classList.add("label");
+
+  const label3 = document.createElement("h2");
+  label3.innerHTML = "Exploding Beef Balls";
+  label3.classList.add("label");
+
+  const label4 = document.createElement("h2");
+  label4.innerHTML = "Heart";
+  label4.classList.add("label");
+
+  image1.classList.add("menu-image");
+  image2.classList.add("menu-image");
+  image3.classList.add("menu-image");
+  image4.classList.add("menu-image");
+
+  image1.src = "./images/image1.jpg";
+  image2.src = "./images/image2.jpeg";
+  image3.src = "./images/image3.jpg";
+  image4.src = "./images/image4.jpg";
 
   content.appendChild(tabContent);
   tabContent.appendChild(title);
   tabContent.appendChild(menu);
 
-  menu.appendChild(imageOne);
+  menu.appendChild(div1);
+  menu.appendChild(div2);
+  menu.appendChild(div3);
+  menu.appendChild(div4);
 
-  menu.appendChild(imageTwo);
-  menu.appendChild(imageThree);
-  menu.appendChild(imageFour);
+  div1.appendChild(image1);
+  div1.appendChild(label1);
+  div2.appendChild(image2);
+  div2.appendChild(label2);
+  div3.appendChild(image3);
+  div3.appendChild(label3);
+  div4.appendChild(image4);
+  div4.appendChild(label4);
 }
 
 export default createMenu;
