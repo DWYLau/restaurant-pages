3c267764b20deea4946fe66b42b13f6e01675f4b
Derek <dereklau15@gmail.com>
2023-05-27 17:58:37 +0900

    Create img elements

--- src/menu.js
+++ src/menu.js
@@ -8,8 +8,33 @@ function createMenu() {
   title.innerHTML = "MENU";
   title.classList.add("heading");
 
+  const menu = document.createElement("div");
+  menu.classList.add("menu-container");
+
+  const imageOne = document.createElement("img");
+  const imageTwo = document.createElement("img");
+  const imageThree = document.createElement("img");
+  const imageFour = document.createElement("img");
+
+  imageOne.classList.add("menu-image");
+  imageTwo.classList.add("menu-image");
+  imageThree.classList.add("menu-image");
+  imageFour.classList.add("menu-image");
+
+  imageOne.src = "./images/image1.jpg";
+  imageTwo.src = "./images/image2.jpeg";
+  imageThree.src = "./images/image3.jpg";
+  imageFour.src = "./images/image4.jpg";
+
   content.appendChild(tabContent);
   tabContent.appendChild(title);
+  tabContent.appendChild(menu);
+
+  menu.appendChild(imageOne);
+
+  menu.appendChild(imageTwo);
+  menu.appendChild(imageThree);
+  menu.appendChild(imageFour);
 }
 
 export default createMenu;
